741eacc7feaa7ed5c2b17d07bfe2f936829488fd
tfarmel <tfarmel@yahoo.fr>
2018-11-27 19:59:01 +0100

    ajout du repo

--- src/main/java/com/tfarmel/entities/EtudiantRepository.java
+++ src/main/java/com/tfarmel/entities/EtudiantRepository.java
@@ -1,7 +1,9 @@
 package com.tfarmel.entities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
 
 }
